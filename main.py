c11771217ed4fbb4023b500757a7e1827ff512d7
Git-Hub-xD <166714901+Git-Hub-xD@users.noreply.github.com>
2024-11-29 15:35:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
 
-API_ID = ""
-API_HASH = ""
-BOT_TOKEN = ""
+API_ID = "21989020"
+API_HASH = "3959305ae244126404702aa5068ba15c"
+BOT_TOKEN = "7410194228:AAGyVEIgppL2tusKBIG_f-PI0XMwuD4uY1Y"
 
 Pyxen = Client(
   name="pyxn",
