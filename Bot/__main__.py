d20028f0ade60dc74e38d7aef20d246c1c5e3620
Git-Hub-xD <166714901+Git-Hub-xD@users.noreply.github.com>
2024-12-02 12:19:34 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,4 +1,10 @@
-from .main import app
+import sys
+import os
+
+# Ensure the project root is in the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from main import app
 
 if __name__ == "__main__":
     app.run()
