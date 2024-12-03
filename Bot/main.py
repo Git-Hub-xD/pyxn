8fe803a3510e36965c0b29a223f082c88857d49a
Git-Hub-xD <166714901+Git-Hub-xD@users.noreply.github.com>
2024-12-03 13:23:10 +0530

    Update main.py

--- Bot/main.py
+++ Bot/main.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from Bot.user_data import create_db, add_user, get_user
 from Bot.flood_control import check_flood
 from Bot.leveling import level_up
+from database.db_manager import ensure_user_exists, get_user
 
 API_ID = "21989020"
 API_HASH = "3959305ae244126404702aa5068ba15c"
