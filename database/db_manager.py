14dd06e239ad63d2dad7463b8931a4e2e633cce3
Git-Hub-xD <166714901+Git-Hub-xD@users.noreply.github.com>
2024-12-03 14:12:20 +0530

    Update db_manager.py

--- database/db_manager.py
+++ database/db_manager.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-from database import schema.sql
+from database import schema
 
 # Path to the SQLite database
 DB_PATH = os.path.join(os.path.dirname(__file__), "user_data.db")
