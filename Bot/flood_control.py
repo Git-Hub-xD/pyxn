18eb2752e5b1a6bdd123dfeed76ee3c60439d096
Git-Hub-xD <166714901+Git-Hub-xD@users.noreply.github.com>
2024-12-03 12:58:03 +0530

    Update flood_control.py

--- Bot/flood_control.py
+++ Bot/flood_control.py
@@ -6,7 +6,7 @@ def check_flood(user_id):
     current_time = time.time()
     if user_id in user_last_command_time:
         last_time = user_last_command_time[user_id]
-        if current_time - last_time < 2:  # 5 seconds cooldown
+        if current_time - last_time < 1:  # 1 seconds cooldown
             return True
     user_last_command_time[user_id] = current_time
     return False
