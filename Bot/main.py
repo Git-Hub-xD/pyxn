b17ea2610810d41b0bd39d3a252723c5647e8aa3
Git-Hub-xD <166714901+Git-Hub-xD@users.noreply.github.com>
2024-12-05 13:35:59 +0530

    Update main.py

--- Bot/main.py
+++ Bot/main.py
@@ -3,14 +3,14 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Bot.flood_control import check_flood
 from Bot.leveling import level_up
-from database.db_manager import create_db, add_user, ensure_user_exists, get_user, update_points, update_level, update_health, update_user_data, connect_db
+from database.db_manager import create_db, add_user, ensure_user_exists, get_user, update_points, update_level, update_health
 
 API_ID = "21989020"
 API_HASH = "3959305ae244126404702aa5068ba15c"
 BOT_TOKEN = "8141351816:AAG1_YB0l88X0SLAHnos9iODdZuCdNEfuFo"
 
 app = Client(
-  name="pyxn",
+  name="Kaisen Ranking Bot",
   api_id=API_ID,
   api_hash=API_HASH,
   bot_token=BOT_TOKEN
